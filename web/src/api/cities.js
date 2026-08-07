25141da0e9ee9b28707c41235ee38f8d22aa624a
albert <alberthaciverdiyev55@gmail.com>
2026-08-07 13:21:01 +0400

    Remove Vue-based web client, including components, layouts, stores, API services, i18n setup, and related assets. Transition to .NET MVC infrastructure.

--- web/src/api/cities.js
+++
@@ -1,7 +0,0 @@
-import http from './http'
-
-export const citiesApi = {
-  getPage(page = 1, pageSize = 100, lang = 'az') {
-    return http.get('/cities', { params: { page, pageSize, lang } })
-  },
-}
